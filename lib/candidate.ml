6fe49a877cbcd2afdfbd08f53cbea75049a55fe1
Jeffas <dev@jeffas.io>
2020-04-27 22:11:45 +0100

    Add handling of append_entries_responses in candidate

--- lib/candidate.ml
+++ lib/candidate.ml
@@ -12,8 +12,16 @@ struct
     in
     (S.Follower s, [])
 
-  let handle_append_entries_response (s : S.candidate) (_res : Ae.res) =
-    (S.Candidate s, [])
+  let handle_append_entries_response (s : S.candidate) (res : Ae.res) =
+    if res.term > s.persistent.current_term then
+      let s =
+        let persistent =
+          S.make_persistent ~current_term:res.term ~log:s.persistent.log ()
+        in
+        S.make_follower ~server:s.server ~persistent ~volatile:s.volatile
+      in
+      (S.Follower s, [])
+    else (* ignore this response *) (S.Candidate s, [])
 
   let handle_request_votes_request (s : S.candidate) _rv =
     let s =
@@ -34,11 +42,13 @@ struct
     else
       let proposed_term, votes = s.votes_received in
       if res.term = proposed_term && res.vote_granted then
+        (* need to update the vote count *)
         let s =
           S.make_candidate
             ~votes_received:(proposed_term, votes + 1)
             ~server:s.server ~volatile:s.volatile ~persistent:s.persistent
         in
+        (* check the updated vote count for a majority *)
         (S.Candidate s, [])
       else
         (* ignore the vote since it is for an incorrect term, maybe a previous vote *)
