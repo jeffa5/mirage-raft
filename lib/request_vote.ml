46155a5239308b778ce9484ddf446900cd7f4b44
Jeffas <dev@jeffas.io>
2020-05-14 22:35:29 +0100

    Add request_vote

---
+++ lib/request_vote.ml
@@ -0,0 +1,28 @@
+module type S = sig
+  type address [@@deriving sexp]
+
+  type args = {
+    term : int;
+    candidate_id : int;
+    last_log_index : int;
+    last_log_term : int;
+  }
+  [@@deriving make, sexp]
+
+  type res = { id : int; term : int; vote_granted : bool }
+  [@@deriving make, sexp]
+
+  val send : address -> args -> unit Lwt.t
+  (** [request_vote] is invoked by candidates to gather votes
+ *
+ *  Arguments
+ *  - [term]: candidate's term
+ *  - [candidate_id]: candidate requesting vote
+ *  - [last_log_index]: index of candidate's last log entry
+ *  - [last_log_term]: term of candidate's last log entry
+ *
+ *  Results
+ *  - [term]: current term, for candidate to update itself
+ *  - [vote_granted]: true means candidate received vote
+ * *)
+end
